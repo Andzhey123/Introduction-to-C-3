fcf6b185706dfe6be6f6150846541b3c672f5252
Andzhey123 <beginov.a@gmail.com>
2022-08-17 15:02:17 +0300

    Добавлено решение по 19 задаче

---
+++ Task19/Program.cs
@@ -0,0 +1,32 @@
+﻿bool CheckPalindrome(int number)
+{
+    int digit1 = number / 10000;
+    int digit5 = number % 10;
+
+    if (digit1 != digit5)
+        return false;
+
+    int digit2 = number / 1000 % 10;
+    int digit4 = number % 100 / 10;
+
+    if (digit2 != digit4)
+        return false;
+
+    return true;
+}
+
+Console.WriteLine("Введите 5-значное число для проверки");
+var numberString = Console.ReadLine();
+var number = int.Parse(numberString!);
+
+if (number < 10_000 && number >= 100_000)
+{
+    return;
+}
+
+var result = CheckPalindrome(number);
+
+if (result == true)
+    System.Console.WriteLine("Число является палиндромом");
+else
+    Console.WriteLine("Не является");
