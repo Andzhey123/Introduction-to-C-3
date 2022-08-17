13a0c3fdbb8bf15c98fcb21fce2ba6c9cad492d0
Andzhey123 <beginov.a@gmail.com>
2022-08-17 16:47:47 +0300

    Добавлено решение по 23 задаче

---
+++ Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("\n Задача 23");
+Decision:
+Console.WriteLine("Введите положительное число");
+var numberString = Console.ReadLine();
+var number = int.Parse(numberString!);
+int count = 1;
+if (number < 0)
+{
+    Console.WriteLine("Вы ввели отрицательное число. Программа ничего не вычислит");
+    goto Decision;
+}
+if (number == 0)
+{
+    Console.WriteLine("Вы ввели ноль. Программа ничего не вычислит");
+    goto Decision;
+}
+else
+{
+    Console.Write($"Кубы всх чисел от 1 до {+number}: ");
+    while (count <= number)
+    {
+        Console.Write(Math.Pow(count, 3) + " ");
+        count += 1;
+    }
+}
